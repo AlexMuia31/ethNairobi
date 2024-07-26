808a6dfc13ea63df28d97e276aca665c2d8b04da
AlexMuia31 <alexmuia31@gmail.com>
2024-07-26 23:09:49 +0300

    Gsap for the header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,7 +1,45 @@
-import React from "react";
+import { Box, Grid, Toolbar, Typography } from "@mui/material";
+import React, { useEffect } from "react";
+import SplitType from "split-type";
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
 
 const Hero = () => {
-  return <div>Hero</div>;
+  useEffect(() => {
+    const text = new SplitType("#my-text");
+    gsap.from(text.chars, {
+      opacity: 0,
+      y: 50,
+      stagger: 0.1,
+      ease: "elastic.out(1,0,3)",
+      duration: 1,
+      transformOrigin: "left top",
+    });
+  }, []);
+
+  return (
+    <Box>
+      <Toolbar />
+      <Grid container sx={{ minHeight: "50vh" }}>
+        <Grid item md={6} xs={12}>
+          <Box sx={{ display: "flex", alignItems: "center", height: "100%" }}>
+            {" "}
+            <Typography
+              id="my-text"
+              sx={{
+                fontFamily: "Poppins",
+                color: "#fff",
+                fontSize: { xs: "30px", md: "80px", sm: "40px" },
+              }}
+            >
+              The Largest Ethereum and Web3 Event in Nairobi, Kenya
+            </Typography>
+          </Box>
+        </Grid>
+        <Grid item md={6} xs={12}></Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export default Hero;
